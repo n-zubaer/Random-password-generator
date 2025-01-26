c463ed256efad13759628f34b2a5ac72b20b6ca4
n-zubaer <nafizzubaer95@gmail.com>
2025-01-26 23:25:51 +0600

    random password generator

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const passwordBox = document.querySelector(".password");
+const copyBtn = document.querySelector(".copyBtn");
+const passBtn = document.querySelector(".passBtn");
+
+const length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowecase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|{}[]<>/-=";
+
+const allchars = upperCase + lowecase + number + symbol;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowecase[Math.floor(Math.random() * lowecase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    password += allchars[Math.floor(Math.random() * allchars.length)];
+  }
+
+  passwordBox.value = password;
+}
+
+passBtn.addEventListener("click", createPassword);
+
+copyBtn.addEventListener("click", () => {
+  passwordBox.select();
+  navigator.clipboard.writeText(passwordBox.value);
+});
